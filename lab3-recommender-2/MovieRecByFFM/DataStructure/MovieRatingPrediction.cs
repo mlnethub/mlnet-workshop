f92d6d24c67af217e469e6c28aabc929bd379afe
Randy Park <randyp@smu.edu>
2019-09-24 15:49:41 -0400

    Added Lab3 Recommender-2

---
+++ lab3-recommender-2/MovieRecByFFM/DataStructure/MovieRatingPrediction.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MovieRecByFFM.DataStructures
+{
+    public class MovieRatingPrediction
+    {
+        public bool PredictedLabel;
+
+        public float Score;
+    }
+}
