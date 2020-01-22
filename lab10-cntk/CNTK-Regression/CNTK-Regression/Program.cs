2f0db30b6a258424b0885ab7c142f130ca6c1113
Randy Park <randyp@smu.edu>
2020-01-22 09:37:00 -0500

    Added new ML.Net hands-on labs

---
+++ lab10-cntk/CNTK-Regression/CNTK-Regression/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CNTK_Regression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
